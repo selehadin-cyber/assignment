9e123afdcb4c2f9149acd78e6ec13d793f79a6f0
selehadin-cyber <sele4416@gmail.com>
2022-04-23 22:28:22 +0300

    added types to styled components

--- frontend/src/components/NoteInput.tsx
+++ frontend/src/components/NoteInput.tsx
@@ -1,57 +1,79 @@
-import { ChangeEvent, useEffect, useState } from "react"
+import { ChangeEvent, useEffect, useState } from "react";
+import styled from "styled-components";
 
 interface NoteInputProps {
-  addNote(title: string, description: string): void
+  addNote(title: string, description: string): void;
 }
 
+const StyledNoteCreateSection = styled.div`
+padding-left: 3.5rem;
+padding-right: 3.5rem;
+background-color: rgba(0,0,0,.75);
+margin: 0 auto;
+@media (min-width: 768px)
+ {
+    max-width: 28rem;
+}
+`
 
-const NoteInput: React.FC <NoteInputProps>= ({ addNote }) => {
+const StyledInput = styled.input`
+  outline: 2px solid transparent;
+  outline-offset: 2px;
+  width: 100%;
+  border-radius: 0.25rem;
+  --tw-bg-opacity: 1;
+  background-color: rgb(51 51 51 / var(--tw-bg-opacity));
+  padding: 0.875rem 1.25rem;
+  background-color: white
+`;
 
-  const [title, setTitle] = useState("")
-  const [descriptionText, setDescriptionText] = useState("")
+const NoteInput: React.FC<NoteInputProps> = ({ addNote }) => {
+  const [title, setTitle] = useState("");
+  const [descriptionText, setDescriptionText] = useState("");
 
   useEffect(() => {
-    console.log("usedefffect")
-  }, [title])
-  
+    console.log("usedefffect");
+  }, [title]);
 
-  const updateNote = (event: ChangeEvent<HTMLInputElement>) => {
-    setTitle(event.target.value)
-  }
+  const updateTitle = (event: ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value);
+  };
 
-  const updateNoteD = (event: ChangeEvent<HTMLInputElement>) => {
-    setDescriptionText(event.target.value)
-  }
+  const updateDescription = (event: ChangeEvent<HTMLInputElement>) => {
+    setDescriptionText(event.target.value);
+  };
 
   const addNoteClick = () => {
     addNote(title, descriptionText);
-    setTitle("")
-    setDescriptionText("")
-  }
+    setTitle("");
+    setDescriptionText("");
+  };
 
   return (
-    <div>
-        <input
-        type="text"
-        name=""
-        value={title}
-        id=""
-        placeholder="description here"
-        onChange={updateNote}
-      />
-      <input
-        type="text"
-        name=""
-        value={descriptionText}
-        id=""
-        placeholder="Notes here"
-        onChange={updateNoteD}
-      />
-      <button onClick={addNoteClick}>
-        save
-      </button>
-    </div>
-  )
-}
+    
+      <StyledNoteCreateSection>
+        <StyledInput
+          type="text"
+          name=""
+          value={title}
+          id=""
+          placeholder="Title here"
+          onChange={updateTitle}
+        />
+        <StyledInput
+          type="text"
+          name=""
+          value={descriptionText}
+          id=""
+          placeholder="Description here"
+          onChange={updateDescription}
+        />
+      
 
-export default NoteInput
+      <button onClick={addNoteClick}>save</button>
+      </StyledNoteCreateSection>
+   
+  );
+};
+
+export default NoteInput;
